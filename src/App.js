6c4da77385897490921b6e20f0ddfbf7398148e2
Yug-soni <yugsoni8946842393@gmail.com>
2021-06-28 09:17:45 +0530

    using the withRouter HOC, and can directly access the props of top component and added navigation url links.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import HomePage from "./pages/home/homepage.component";
-
+import { Switch, Route } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <div>
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+        </Switch>
       </div>
     );
   }
