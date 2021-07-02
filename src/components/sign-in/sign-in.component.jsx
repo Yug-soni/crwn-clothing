8975b5d07ffade0c674ada974f8a4d806b8fdc02
Yug-soni <yugsoni8946842393@gmail.com>
2021-07-02 14:47:32 +0530

    Finished the sign and sign working fully integreated with fireabase with no errors.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import "./sign-in.styles.scss";
 import FormInput from "./../form-input/form-input.component";
 import CustomButton from "./../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "./../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "./../../firebase/firebase.utils";
 export class SignIn extends React.Component {
   constructor(props) {
     super(props);
@@ -16,12 +16,20 @@ export class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({
-      email: "",
-      password: "",
-    });
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: "",
+        password: "",
+      });
+    } catch (error) {
+      console.log(`Error while creating the user :- ${error}`);
+    }
   };
 
   handleChange = event => {
